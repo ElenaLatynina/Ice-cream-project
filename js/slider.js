eb3748e02fec05c0246b35e6e2ad95ce738cb30b
ElenaLatynina <latynina.elena.v@gmail.com>
2022-05-23 13:21:43 +0300

    slider

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,9 @@
-$('.single-item').slick();
+$('.reviews__list').slick(
+    {dots:true,
+    nextArrow:".btn__next",
+    prevArrow:".btn__prev",
+    autoplay:true,
+    aotoplaySpeed:300,
+
+}
+);
